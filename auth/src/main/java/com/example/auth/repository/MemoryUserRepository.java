51e0318a161bdb51a5b691f9c8f97327c878292f
유성민 <growth_s@naver.com>
2023-01-26 23:51:06 +0900

    jar

---
+++ auth/src/main/java/com/example/auth/repository/MemoryUserRepository.java
@@ -0,0 +1,9 @@
+package com.example.auth.repository;
+
+import com.example.auth.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public class MemoryUserRepository {
+}
